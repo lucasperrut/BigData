40d5e594359f71c81156ee3a7e2c74468022ebc1
Lucas Perrut <lperrut@mag.com.br>
2021-12-01 17:56:02 -0300

    Adiciona api

---
+++ API/Dtos/NewspaperDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace API.Dtos
+{
+    public class NewspaperDto
+    {
+        public DateTime date { get; set; }
+        public string hour { get; set; }
+        public string text { get; set; }
+        public string source { get; set; }
+        public string username { get; set; }
+        public string hashtags { get; set; }
+
+    }
+}
